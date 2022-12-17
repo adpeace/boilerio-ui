ba8fb0b676b9cd22f8deda0a28053446f50fa4bf
Andrew Peace <andrew.peace@gmail.com>
2022-12-17 17:53:12 +0000

    Sign in with Google Identity Services for Web.
    The previous Google Sign-in library was deprecated and will stop working
    in March 2023.

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import { GoogleLogin } from 'react-google-login';
+import { GoogleLogin } from '@react-oauth/google';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
+import { Container } from '@material-ui/core';
 
 import axios from 'axios';
 const axios_inst = axios.create({
@@ -27,7 +28,7 @@ export function Login(props) {
         // Log into backend with the ID token as credential:
         async function complete_auth() {
             var formData = new FormData();
-            formData.set("id_token", d.tokenObj.id_token);
+            formData.set("id_token", d.credential);
             try {
                 await axios_inst.post('api/me', formData);
                 props.setAuthRequired(false);
@@ -49,22 +50,19 @@ export function Login(props) {
 
     return (
         <div className={classes.content}>
-            <Typography
-                variant="h5"
-                gutterBottom={true}
-                >Please log in to use BoilerIO.</Typography>
-            <GoogleLogin
-                clientId="338550409596-dm6oma99fn02brpoi106pemcujs161al.apps.googleusercontent.com"
-                buttonText="Log in with Google"
-                onSuccess={loginSuccess}
-                onFailure={loginFailure}
-                cookiePolicy={"single_host_origin"}
-                redirectUri="postmessage"
-                scope="openid"
-            />
-            <Snackbar open={loginError} autoHideDuration={10000} onClose={() => setLoginError(false)}>
-                <Alert elevation={6} severity="error" variant="filled">Error logging in</Alert>
-            </Snackbar>
+            <Container maxWidth="sm">
+                <Typography
+                    variant="h5"
+                    gutterBottom={true}
+                    >Please log in to use BoilerIO.</Typography>
+                <GoogleLogin useOneTap
+                    onSuccess={loginSuccess}
+                    onFailure={loginFailure}
+                />
+                <Snackbar open={loginError} autoHideDuration={10000} onClose={() => setLoginError(false)}>
+                    <Alert elevation={6} severity="error" variant="filled">Error logging in</Alert>
+                </Snackbar>
+            </Container>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <GoogleOAuthProvider clientId='752350466610-gce0q11es3ap63hg0vn42p84p184pf0g.apps.googleusercontent.com'>
+        <App />
+    </GoogleOAuthProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
